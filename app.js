efde6e01058c9fffd32f05e44ceadb123e127793
SDDS Banjo <168935997+sddsBanjo@users.noreply.github.com>
2026-08-31 21:34:17 +0000

    feat: exercise 3

--- app.js
+++ app.js
@@ -10,3 +10,20 @@ function ex2() {
     const h3 = document.getElementById('h3_ex2');
     h3.innerHTML = inputA + inputB;
 };
+
+function ex3() {
+    const inputA = Number(document.getElementById('inpA_ex3').value);
+    const inputB = Number(document.getElementById('inpB_ex3').value);
+    const inputC = Number(document.getElementById('inpC_ex3').value);
+    const inputD = Number(document.getElementById('inpD_ex3').value);
+    const h3 = document.getElementById('h3_ex3');
+    const media = (inputA + inputB + inputC + inputD)/4;
+    let status = '';
+    if (media < 7) {
+        status = 'reprovado';
+    } else {
+        status = 'aprovado';
+    };
+    const string = `Média: ${media} | Situação: ${status}!`;
+    h3.innerHTML = string;
+};
