a98b0447fc8074ac7e3b39150dbbde1582921b79
SDDS Banjo <168935997+sddsBanjo@users.noreply.github.com>
2026-08-31 21:19:22 +0000

    feat: exercise 2

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ function ex1() {
     const h3 = document.getElementById('h3_ex1');
     h3.innerHTML = input.value;
 };
+
+function ex2() {
+    const inputA = Number(document.getElementById('inpA_ex2').value);
+    const inputB = Number(document.getElementById('inpB_ex2').value);
+    const h3 = document.getElementById('h3_ex2');
+    h3.innerHTML = inputA + inputB;
+};
